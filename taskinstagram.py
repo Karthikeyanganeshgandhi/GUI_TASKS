9aa0673d2684a749112fd39f127b913586213ffa
Karthikeyan G <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-12-05 16:47:41 +0530

    Add files via upload

---
+++ taskinstagram.py
@@ -0,0 +1,47 @@
+# from tkinter import *
+# from tkinter import ttk
+
+# user=Tk()
+# user.title("www.instagram/login/page/meta.com")
+
+# label1=Label(user,text="Instagram",fg="black",font=(("Billabong","80")))
+# label1.place(x=500,y=0)
+
+# label2=Label(user,text="E-Mail or Phone Number :",font=(("normal","15")))
+# label2.place(x=600,y=200)
+
+# entry=Entry(user,justify=CENTER,font=(("arial","15")))
+# entry.place(x=599,y=250)
+
+# label3=Label(user,text="Password :",font=(("normal","18")))
+# label3.place(x=660,y=350)
+
+# entry1=Entry(user,justify=CENTER,font=(("arial","15")))
+# entry1.place(x=602,y=410)
+
+# def Login():
+#     print("login successfully")
+
+# btn=Button(user,text="Log In",command=Login,activebackground="silver",fg="white",bg="blue",width=30)
+# btn.place(x=620,y=500)
+
+# label4=Label(user,text="----------------------------------OR--------------------------------",fg="black",font=(("normal","10")))
+# label4.place(x=588,y=550)
+
+# label5=Label(user,text="Log In with FaceBook",fg="blue",font=(("normal","13")))
+# label5.place(x=670,y=600)
+
+# path1=PhotoImage(file="C:/Users/karth/Downloads/OIP (1).png")
+# label_image=Label(user,image=path1,width=30,height=30)
+# label_image.place(x=630,y=595)
+
+# label6=Label(user,text="Forgot PassWord ?",fg="blue",font=(("normal","10")))
+# label6.place(x=680,y=660)
+
+# path2=PhotoImage(file="C:/Users/karth/OneDrive/Pictures/245465857-80df6e7f-b157-4389-911c-176db3871f19.png")
+# my_image=Label(user,image=path2,width=400,height=800)
+# my_image.place(x=0,y=0)
+
+# user.geometry("1400x1200")
+# user.mainloop()
+
---
+++ whatsapp.py
@@ -0,0 +1,30 @@
+# from tkinter import *
+# from tkinter import ttk
+
+# user=Tk()
+# user.title("www.whatsapplogin/signup/page.in")
+# path=PhotoImage(file="C:/Users/karth/OneDrive/Pictures/[UNSET].png")
+# label_image=Label(user,image=path,width=400,height=400)
+# label_image.place(x=150,y=70)
+
+# label1=Label(user,text="Welcome To WhatsApp",fg="black",font=(("normal","30")))
+# label1.place(x=140,y=500)
+
+# label2=Label(user,text="A Simple,reliable and private way to use WhatsApp on your computer.",fg="black",font=(("normal","15")))
+# label2.place(x=60,y=570)
+
+# def getstarted():
+#     print("get started successfully")
+
+# button=Button(user,text="Get Started",command=getstarted,bg="green",activebackground="silver",fg="white",width="20")
+# button.place(x=260,y=630)
+
+# label3=Label(user,text="Version 2.2445.7.0",fg="black",font=(("normal","10")))
+# label3.place(x=276,y=680)
+
+# path1=PhotoImage(file="C:/Users/karth/OneDrive/Pictures/Screenshots/Screenshot 2024-11-21 181906.png")
+# my_image=Label(user,image=path1,width=900,height=900)
+# my_image.place(x=700,y=-24)
+
+# user.geometry("350x220")
+# user.mainloop()
